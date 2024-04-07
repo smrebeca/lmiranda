ce7e4faa039a64c84904496667a1c79390192812
smrebeca <rebecasousam@gmail.com>
2024-04-07 17:21:42 -0300

    changes tags

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+const btnMostrarProjetos = document.querySelector('.btn-mostrar-mais');
+const projetosAntigos = document.querySelectorAll('.projeto:not(.recente)');
+
+let projetosVisiveis = false;
+
+btnMostrarProjetos.addEventListener('click', () => {
+    projetosAntigos.forEach(projetoAntigo => {
+        if (!projetosVisiveis) {
+            projetoAntigo.classList.add('recente');
+            btnMostrarProjetos.textContent = 'Mostrar menos';
+        } else {
+            projetoAntigo.classList.remove('recente');
+            btnMostrarProjetos.textContent = 'Mostrar mais';
+        }
+    });
+
+    projetosVisiveis = !projetosVisiveis;
+});
+
+/* document.addEventListener('DOMContentLoaded', function() {
     const pdfButtons = document.querySelectorAll('.pdf-view');
     
     pdfButtons.forEach(function(button) {
@@ -7,4 +26,4 @@ document.addEventListener('DOMContentLoaded', function() {
             window.open(url, '_blank');
         });
     });
-});
+}); */
